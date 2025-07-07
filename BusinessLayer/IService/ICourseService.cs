ef41e50000c9d45990ffe1fc2663d7d4c2114763
ChauVietKhanhTrungK18 <trungcvkse181536@fpt.edu.vn>
2025-07-07 13:26:14 +0700

    addManageCourse

--- BusinessLayer/IService/ICourseService.cs
+++ BusinessLayer/IService/ICourseService.cs
@@ -11,24 +11,17 @@ namespace BusinessLayer.IService
 {
     public interface ICourseService
     {
-        Task<CourseDto> CreateCourseAsync(CourseCreateDto courseCreateDto);
         Task<CourseDto> GetCourseByIdAsync(int courseId,string userId,string userRole);
         Task<CourseListDto> GetCourseByCourseId(int courseId);
-        Task<IEnumerable<CourseWithEnrollmentStatusDto>> GetAllCoursesAsync(string userRole, string userId);
-        Task<IEnumerable<CourseListDto>> GetAllCoursesForManagerAsync();
-        Task<IEnumerable<CourseListDto>> GetActiveCoursesAsync();
-        Task<IEnumerable<CourseListDto>> GetDraftCoursesAsync();
-        Task<IEnumerable<CourseListDto>> GetInactiveCoursesAsync();
-        Task<bool> CanApproveCourseAsync(int courseId);
-        Task UpdateCourseStatusAsync(int courseId, CourseStatus status);
+        Task<IEnumerable<CourseWithEnrollmentStatusDto>> GetAllCoursesAsync(string userId);
+        Task<IEnumerable<CourseListDto>> GetCoursesByStatusAsync(CourseStatus? status = null);
         Task<IEnumerable<CourseListDto>> GetCoursesInProgressAsync(string userId);
         Task<IEnumerable<CourseListDto>> GetCompletedCoursesAsync(string userId);
         Task<bool> IsUserEnrolledInCourseAsync(string userId, int courseId);
-        Task UpdateCourseAsync(int courseId, CourseUpdateDto courseUpdateDto);
-        Task DeleteCourseAsync(int courseId);
-        Task<IEnumerable<CourseWithEnrollmentStatusDto>> GetCoursesByTopicAsync(CourseTopic topic, string userRole, string userId);
 
-        Task<IEnumerable<CourseWithEnrollmentStatusDto>> SearchCourseAsync(string searchTerm, string userRole, string userId);
+        Task<IEnumerable<CourseWithEnrollmentStatusDto>> GetCoursesByTopicAsync(CourseTopic topic,   string userId);
+
+        Task<IEnumerable<CourseWithEnrollmentStatusDto>> SearchCourseAsync(string searchTerm,string userId);
 
         Task<CourseEnrollment> EnrollInCourseAsync(string userId, int courseId);
         Task<IEnumerable<CourseEnrollmentDto>> GetAllEnrollmentsForCourseAsync(int courseId);
@@ -44,5 +37,19 @@ namespace BusinessLayer.IService
         //getdetailCompleteCourse
         Task<CompletedCourseDetailDto> GetCompletedCourseDetailAsync(int courseId, string userId);
         Task<IEnumerable<CourseListDto>> GetCoursesWithoutSurveyOrInactiveSurveyAsync();
+
+        //Manager
+        Task<CourseDto> CreateCourseAsync(CourseCreateDto courseCreateDto);
+
+        Task<IEnumerable<CourseListDto>> GetAllCoursesForManagerAsync();
+        Task<bool> CanApproveCourseAsync(int courseId);
+        Task UpdateCourseStatusAsync(int courseId, CourseStatus status);
+        Task UpdateCourseAsync(int courseId, CourseUpdateDto courseUpdateDto);
+        Task DeleteCourseAsync(int courseId);
+        Task<IEnumerable<CourseListDto>> SearchCourseForManagerAsync(string searchTerm, CourseStatus? status = null);
+        Task<IEnumerable<CourseListDto>> GetCoursesByTopicForManagerAsync(CourseTopic topic, CourseStatus? status = null);
+
+
+
     }
 }
---
+++ SWP391_Project/Controllers/CourseManagementController.cs
@@ -0,0 +1,140 @@
+﻿using BusinessLayer.IService;
+using DataAccessLayer.Dto.Course;
+using DataAccessLayer.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace SWP391_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseManagementController : ControllerBase
+    {
+        private readonly ICourseService _courseService;
+        public CourseManagementController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Manager")]
+        public async Task<IActionResult> CreateCourse([FromBody] CourseCreateDto courseCreateDto)
+        {
+            var course = await _courseService.CreateCourseAsync(courseCreateDto);
+            if (course == null)
+            {
+                return BadRequest("Failed to create course.");
+            }
+            return Ok(new { Message = "Course created successfully!", CourseId = course.Id });
+        }
+
+        [HttpPut("approve/{courseId}")]
+        [Authorize(Roles = "Manager")]
+        public async Task<IActionResult> ApproveCourse(int courseId)
+        {
+            var course = await _courseService.GetCourseByCourseId(courseId);
+            if (course == null)
+                return NotFound("Course not found.");
+
+            if (course.Status != CourseStatus.Draft)
+                return BadRequest("Only Draft courses can be activated.");
+
+            if (!await _courseService.CanApproveCourseAsync(courseId))
+                return BadRequest("Course must have at least one lesson and one survey before activation.");
+
+            await _courseService.UpdateCourseStatusAsync(courseId, CourseStatus.Active);
+            return Ok(new { Message = "Course is now active and visible to members!" });
+        }
+
+        [HttpPut("{courseId}")]
+        [Authorize(Roles = "Manager")]
+        public async Task<IActionResult> UpdateCourse(int courseId, [FromBody] CourseUpdateDto courseUpdateDto)
+        {
+            await _courseService.UpdateCourseAsync(courseId, courseUpdateDto);
+            return Ok(new { Message = "Course updated successfully!" });
+        }
+
+
+        [HttpDelete("{courseId}")]
+        [Authorize(Roles = "Manager")]
+        public async Task<IActionResult> DeleteCourse(int courseId)
+        {
+            await _courseService.DeleteCourseAsync(courseId);
+            return Ok(new { Message = "Course deleted successfully!" });
+        }
+
+        [HttpGet("draft")]
+        [Authorize(Roles = "Staff,Manager")]
+        public async Task<IActionResult> GetDraftCourses()
+        {
+            var draftCourses = await _courseService.GetCoursesByStatusAsync(CourseStatus.Draft);
+            return Ok(draftCourses);
+        }
+
+        [HttpGet("searchTitle")]
+        public async Task<IActionResult> SearchCourse([FromQuery] string searchTerm, [FromQuery] CourseStatus? status = null)
+        {
+            var courses = await _courseService.SearchCourseForManagerAsync(searchTerm, status);
+            return Ok(courses);
+        }
+
+        [HttpGet("topic/{topic}")]  
+        public async Task<IActionResult> GetCoursesByTopic(CourseTopic topic, [FromQuery] CourseStatus? status = null)
+        {
+            var courses = await _courseService.GetCoursesByTopicForManagerAsync(topic, status);
+            return Ok(courses);
+        }
+
+        [HttpGet("enrollments/{courseId}")]
+        [Authorize(Roles = "Admin,Manager")]
+        public async Task<IActionResult> GetAllEnrollmentsForCourse(int courseId)
+        {
+            var enrollments = await _courseService.GetAllEnrollmentsForCourseAsync(courseId);
+            return Ok(enrollments);
+        }
+
+
+
+        [HttpGet("allCourses/Manager")]
+        [Authorize(Roles = "Manager")]
+        public async Task<IActionResult> GetCourses([FromQuery] CourseStatus? status = null)
+        {
+            var courses = await _courseService.GetCoursesByStatusAsync(status);
+            return Ok(courses);
+        }
+        
+        [HttpGet("courses_without_survey")]
+        [Authorize(Roles = "Manager,Staff")]
+        public async Task<IActionResult> GetCoursesWithoutSurveyOrInactiveSurvey()
+        {
+            var courses = await _courseService.GetCoursesWithoutSurveyOrInactiveSurveyAsync();
+            return Ok(courses);
+        }
+
+        [HttpGet("report/{courseId}")]
+        [Authorize(Roles = "Manager,Admin")]
+        public async Task<IActionResult> GetCourseReport(int courseId)
+        {
+            var report = await _courseService.GetCourseReportAsync(courseId);
+            return Ok(report);
+        }
+
+        [HttpGet("completed-course/{courseId}")]
+        [Authorize(Roles = "Member,Manager")]
+        public async Task<IActionResult> GetCompletedCourseById(int courseId)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var course = await _courseService.GetCompletedCourseDetailAsync(courseId, userId);
+            if (course == null)
+            {
+                return NotFound("You have not complete this course!");
+            }
+
+            return Ok(course);
+        }
+
+    }
+}
