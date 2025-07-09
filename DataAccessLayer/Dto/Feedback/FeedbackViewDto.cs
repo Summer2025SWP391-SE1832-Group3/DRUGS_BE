ff86f5f05be043af7b356024e4d01d042a8fe837
ChauVietKhanhTrungK18 <trungcvkse181536@fpt.edu.vn>
2025-07-09 15:08:27 +0700

    AddFeedbackTb

---
+++ DataAccessLayer/Dto/Feedback/FeedbackViewDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Dto.Feedback
+{
+    public class FeedbackViewDto
+    {
+        public int FeedbackId { get; set; }
+        public string CreateBy { get; set; }
+        public string CourseTitle { get; set; } 
+        public int Rating { get; set; } 
+        public string ReviewText { get; set; } 
+        public DateTime CreatedAt { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
--- DataAccessLayer/IRepository/ICourseReportRepository.cs
+++ DataAccessLayer/IRepository/ICourseReportRepository.cs
@@ -1,5 +1,6 @@
 ﻿using DataAccessLayer.Dto.Course;
 using DataAccessLayer.Dto.Lesson;
+using DataAccessLayer.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,7 @@ namespace DataAccessLayer.IRepository
 {
     public interface ICourseReportRepository
     {
-        Task<CourseReportDto> GetCourseReportAsync(int courseId);
+        Task<IEnumerable<CourseEnrollment>> GetEnrollmentsByCourseIdAsync(int courseId);
         Task<LessonProgressReportDto> GetLessonProgressReportAsync(int courseId);
     }
 
