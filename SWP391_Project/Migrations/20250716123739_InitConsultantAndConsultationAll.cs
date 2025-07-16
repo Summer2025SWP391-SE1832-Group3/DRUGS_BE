29acc839db52f8ec81f6a418587f777414e46c63
ChauVietKhanhTrungK18 <trungcvkse181536@fpt.edu.vn>
2025-07-17 02:17:57 +0700

    pullLongVyAndTest

--- SWP391_Project/Migrations/20250716123739_InitConsultantAndConsultationAll.cs
+++ SWP391_Project/Migrations/20250716123739_InitConsultantAndConsultationAll.cs
@@ -21,31 +21,6 @@ namespace SWP391_Project.Migrations
                 name: "IX_ConsultantWorkingHours_ConsultationRequestId",
                 table: "ConsultantWorkingHours");
 
-            migrationBuilder.DeleteData(
-                table: "AspNetRoles",
-                keyColumn: "Id",
-                keyValue: "280aee5a-cc6f-4e38-a1e3-9c74257230e8");
-
-            migrationBuilder.DeleteData(
-                table: "AspNetRoles",
-                keyColumn: "Id",
-                keyValue: "400eda71-0746-4481-b09d-b7d45800d46c");
-
-            migrationBuilder.DeleteData(
-                table: "AspNetRoles",
-                keyColumn: "Id",
-                keyValue: "b7b29921-44be-471c-89c5-5c2875cba655");
-
-            migrationBuilder.DeleteData(
-                table: "AspNetRoles",
-                keyColumn: "Id",
-                keyValue: "cda350e0-9284-4e67-a4b7-19d984a42b0f");
-
-            migrationBuilder.DeleteData(
-                table: "AspNetRoles",
-                keyColumn: "Id",
-                keyValue: "e2e7fe74-cfc1-484e-8dbd-141aa79a2913");
-
             migrationBuilder.DropColumn(
                 name: "ConsultationRequestId",
                 table: "ConsultantWorkingHours");
@@ -155,18 +130,6 @@ namespace SWP391_Project.Migrations
                 table: "AspNetUsers",
                 type: "int",
                 nullable: true);
-
-            migrationBuilder.InsertData(
-                table: "AspNetRoles",
-                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
-                values: new object[,]
-                {
-                    { "280aee5a-cc6f-4e38-a1e3-9c74257230e8", null, "Manager", "MANAGER" },
-                    { "400eda71-0746-4481-b09d-b7d45800d46c", null, "Member", "MEMBER" },
-                    { "b7b29921-44be-471c-89c5-5c2875cba655", null, "Staff", "STAFF" },
-                    { "cda350e0-9284-4e67-a4b7-19d984a42b0f", null, "Consultant", "CONSULTANT" },
-                    { "e2e7fe74-cfc1-484e-8dbd-141aa79a2913", null, "Admin", "ADMIN" }
-                });
 
             migrationBuilder.CreateIndex(
                 name: "IX_ConsultantWorkingHours_ConsultationRequestId",
